b9b0e9f88d214e4a8d49d69eff22f4d630b44ed0
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2024-10-06 18:37:14 -0300

    Updating packages

--- src/components/InputKeyboard.tsx
+++ src/components/InputKeyboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { calcProp, replaceOps } from "../lib/dataCalc"; 
 import style from "../styles/keyboard.module.scss"
 import DialogBox from "./Dialog";
